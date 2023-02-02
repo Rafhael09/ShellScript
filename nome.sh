e7aa9215d31d8cd57069d7394339fd446a547a7c
Rafhael de Moraes <124003404+Rafhael09@users.noreply.github.com>
2023-02-02 19:17:20 -0300

    Add files via upload

---
+++ nome.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################################
+
+#Nome do script: nome.sh
+#Descrição: Pergunte em caixa
+#Versão: 1.0
+#Lincença: OpenSource
+#Autor: Rafhael de Moraes Barbosa
+#E-mail: rafhaeltec@gmail.com
+
+#################################
+
+nome=$(dialog --inputbox "Qual é o seu nome?" 10 60 3>&1 1>&2 2>&3)
+
+dialog --title "Resultado" --msgbox "Olá, $nome!" 10 60
