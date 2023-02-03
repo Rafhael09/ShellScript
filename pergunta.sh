8deda23d7518570d069d368b5d8e9bce17e0b821
Rafhael de Moraes <124003404+Rafhael09@users.noreply.github.com>
2023-02-02 21:18:55 -0300

    Add files via upload

---
+++ pergunta.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#################################
+
+#Nome do script: perguntas.sh
+#Descrição: Faz perguntas
+#Versão: 1.0
+#Lincença: OpenSource
+#Autor: Rafhael de Moraes Barbosa
+#E-mail: rafhaeltec@gmail.com
+
+#################################
+
+# This is comment
+
+# Defining a variable
+echo "Olâ, qual seu nome?"
+# Reading input	
+read NAME
+# Defining a variable
+GREETINGS="! Tudo bem com você?"
+echo $NAME $GREETINGS
